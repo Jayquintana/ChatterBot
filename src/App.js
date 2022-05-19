bebd6fc127b6d5d916e11294df49d499ddab0e26
Jacob Quintana <jacobquintaa@gmail.com>
2022-05-19 11:43:50 -0600

    style and add text box

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ function App() {
   return (
     <div className="App">
       <h1>Chatter Bot</h1>
+      <div>
+        <input type="text"required/>
+        <p>Ask Question</p>
+      </div>  
     </div>
   );
 }
