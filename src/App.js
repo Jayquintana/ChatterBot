011442a0024efc7e8455d0345881870a2e2b270e
Jacob Quintana <jacobquintaa@gmail.com>
2022-05-20 02:40:29 -0600

    ✅ build simple idea box

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
 import QuestionBox from '/Users/jayp/Desktop/application-projects/chatter-box/src/Components/QuestionsSection/QuestionBox.js'
+import SavedResponses from '/Users/jayp/Desktop/application-projects/chatter-box/src/Components/ResponseSection/SavedResponses.js'
+import React, { useState } from 'react';
+
 function App() {
+  const [userQuestion, setUserQuestion] = useState('')
+  const [botAnswer, setBotAnswer] = useState('')
+  const [responses, setResponses] = useState([])
+
+    const displayResponse = () => {
+      let result = responses.map((answer, i) => {
+        return (<SavedResponses key={i} botAnswer={answer.botAnswer}  question={answer.question} />)
+      })
+      console.log(result);
+      return result
+    }
+
   return (
     <div className="App">
-      <QuestionBox />
+      <QuestionBox setResponses={setResponses} responses={responses} botAnswer={botAnswer} userQuestion={userQuestion} setUserQuestion={setUserQuestion} />
+      {displayResponse()}
     </div>
   )
 }
--- src/Components/QuestionsSection/QuestionBox.js
+++ src/Components/QuestionsSection/QuestionBox.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import './QuestionBox.css'
 
-const QuestionBox = () => {
+const QuestionBox = ({userQuestion, setUserQuestion, responses, botAnswer, }) => {
+
+
+const getInput = (e) => {
+ setUserQuestion(e.target.value)
+}
+
+const addResponse = () => {
+ let saveResponse = {
+  id: responses.length + 1,
+  question: userQuestion,
+  botAnswer: botAnswer
+ }
+ if (saveResponse.question) {
+  responses.push(saveResponse)
+ }
+ setUserQuestion('')
+}
+
   return (
    <> 
     <h1>Chatter Bot</h1>
       <div className='text-container'>
-        <input className='text-box' type="text"required/>
-        <p className='askQuestion'>Ask A Question</p>
+        <input value={userQuestion} onChange={e => getInput(e)} className='text-box' placeholder='Whats todays date?' type="text"required/>
+        <p onClick={addResponse} className='ask-question'>Ask A Question</p>
       </div>  
    </>
   )
